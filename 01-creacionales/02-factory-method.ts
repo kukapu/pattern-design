f8a3edcf9b445f3e2a70e5ec24b2ec2fcfce4175
kukapu <susokukapu@gmail.com>
2025-12-14 09:14:56 +0100

    2 factory methos

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -12,3 +12,83 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+import { COLORS } from "../helpers/colors";
+
+interface Hamburger {
+  prepare(): void;
+}
+
+class ChickenHamburger implements Hamburger {
+  prepare(): void {
+    console.log('Preparando el pollo');
+  }
+}
+
+class BeefHamburger implements Hamburger {
+  prepare(): void {
+    console.log('Preparando el carne');
+  }
+}
+
+class BeanHamburger implements Hamburger {
+  prepare(): void {
+    console.log('Preparando el frijoles');
+  }
+}
+
+
+abstract class Restaurant {
+  abstract createHamburger(): Hamburger;
+
+  orderHamburger(): void {
+    const hamburger = this.createHamburger();
+    hamburger.prepare();
+  }
+}
+
+
+class ChickenRestaurant extends Restaurant {
+  override createHamburger(): Hamburger {
+    return new ChickenHamburger();
+  }
+}
+
+class BeefRestaurant extends Restaurant {
+  override createHamburger(): Hamburger {
+    return new BeefHamburger();
+  }
+}
+
+class BeanRestaurant extends Restaurant {
+  override createHamburger(): Hamburger {
+    return new BeanHamburger();
+  }
+}
+
+function main() {
+  let restaurant: Restaurant | null = null;
+
+  const type = prompt('¿Qué tipo de hamburguesa deseas? (chicken/beef/bean)');
+
+  if (type === 'chicken') {
+    restaurant = new ChickenRestaurant();
+  }
+
+  if (type === 'beef') {
+    restaurant = new BeefRestaurant();
+  }
+
+  if (type === 'bean') {
+    restaurant = new BeanRestaurant();
+  }
+
+  if (!restaurant) {
+    console.log('Tipo de hamburguesa no válido');
+    return;
+  }
+
+  restaurant.orderHamburger();
+}
+
+main();
--- 01-creacionales/02.2-factory-method.ts
+++ 01-creacionales/02.2-factory-method.ts
@@ -16,11 +16,11 @@
  * 	!Descripción:
   1.	Completen las clases SalesReport e InventoryReport para implementar 
       la interfaz Report, generando el contenido de cada reporte en el método generate.
-	  
+    
   2.	Implementen las clases SalesReportFactory e InventoryReportFactory 
       para crear instancias de SalesReport y InventoryReport, respectivamente.
 
-	3.	Prueben el programa generando diferentes tipos de reportes usando
+  3.	Prueben el programa generando diferentes tipos de reportes usando
       el prompt para seleccionar el tipo de reporte.
  */
 
@@ -35,19 +35,26 @@ interface Report {
 // Implementar SalesReport e InventoryReport
 
 class SalesReport implements Report {
-  // TODO: implementar el método e imprimir en consola:
-  // 'Generando reporte de ventas...'
+  generate(): void {
+    console.log('Generando reporte de ventas...');
+  }
 }
 
 class InventoryReport implements Report {
-  // TODO: implementar el método e imprimir en consola:
-  // 'Generando reporte de inventario...'
+  generate(): void {
+    console.log('Generando reporte de inventario...');
+  }
 }
 
+class RRHHReport implements Report {
+  generate(): void {
+    console.log('Generando reporte de RRHH...');
+  }
+}
 // 3. Clase Base ReportFactory con el Método Factory
 
 abstract class ReportFactory {
-  abstract createReport(): Report;
+  protected abstract createReport(): Report;
 
   generateReport(): void {
     const report = this.createReport();
@@ -58,31 +65,48 @@ abstract class ReportFactory {
 // 4. Clases Concretas de Fábricas de Reportes
 
 class SalesReportFactory extends ReportFactory {
-  createReport(): Report {
-    throw new Error('Method not implemented.');
+  protected createReport(): Report {
+    return new SalesReport();
   }
 }
 
 class InventoryReportFactory extends ReportFactory {
-  createReport(): Report {
-    throw new Error('Method not implemented.');
+  protected createReport(): Report {
+    return new InventoryReport();
+  }
+}
+
+class RRHHReportFactory extends ReportFactory {
+  protected createReport(): Report {
+    return new RRHHReport();
   }
 }
 
 // 5. Código Cliente para Probar
 
 function main() {
-  let reportFactory: ReportFactory;
+  let reportFactory: ReportFactory | null = null;
 
   const reportType = prompt(
-    '¿Qué tipo de reporte deseas? %c(sales/inventory)',
+    '¿Qué tipo de reporte deseas? %c(sales/inventory/rrhh)',
     COLORS.red
   );
 
   if (reportType === 'sales') {
     reportFactory = new SalesReportFactory();
-  } else {
+  }
+
+  if (reportType === 'inventory') {
     reportFactory = new InventoryReportFactory();
+  }
+
+  if (reportType === 'rrhh') {
+    reportFactory = new RRHHReportFactory();
+  }
+
+  if (!reportFactory) {
+    console.log('Tipo de reporte no válido');
+    return;
   }
 
   reportFactory.generateReport();
